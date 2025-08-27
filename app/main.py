f13d7652611436fbffb5980b99803ef5937d94c1
hemant11ks <ksingh1617@gmail.com>
2025-08-27 13:07:13 +0530

    added logic for start anchor1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ DIGIT = 1
 ALNUM = 2
 POS_CHAR_GROUP = 3
 NEG_CHAR_GROUP = 4
-START_ANCHOR = 5   # NEW
+START_ANCHOR = 5  # new
 
 numbers = list("0123456789")
 alphanum = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
@@ -13,38 +13,19 @@ alphanum = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_
 
 def match_one(input_line, token, pattern, at_start):
     if token == DIGIT:
-        if input_line and input_line[0] in numbers:
-            return input_line[1:]
-        else:
-            return False
+        return input_line[1:] if input_line and input_line[0] in numbers else False
     elif token == ALNUM:
-        if input_line and input_line[0] in alphanum:
-            return input_line[1:]
-        else:
-            return False
+        return input_line[1:] if input_line and input_line[0] in alphanum else False
     elif token == LITERAL:
-        if input_line and input_line[0] == pattern[0]:
-            return input_line[1:]
-        else:
-            return False
+        return input_line[1:] if input_line and input_line[0] == pattern[0] else False
     elif token == POS_CHAR_GROUP:
         inclusion = pattern[1 : pattern.index("]")]
-        if input_line and input_line[0] in list(inclusion):
-            return input_line[1:]
-        else:
-            return False
+        return input_line[1:] if input_line and input_line[0] in inclusion else False
     elif token == NEG_CHAR_GROUP:
         exclusion = pattern[2 : pattern.index("]")]
-        if input_line and input_line[0] not in list(exclusion):
-            return input_line[1:]
-        else:
-            return False
+        return input_line[1:] if input_line and input_line[0] not in exclusion else False
     elif token == START_ANCHOR:
-        # Only matches at very beginning
-        if at_start:
-            return input_line
-        else:
-            return False
+        return input_line if at_start else False
     else:
         return False
 
@@ -65,7 +46,7 @@ def determine_next_token(pattern):
 
 
 def match_pattern(input_line, pattern, latest_token_satisfied):
-    at_start = True  # Track if we are at beginning
+    at_start = True
     while True:
         if pattern == "":
             return True
@@ -80,7 +61,7 @@ def match_pattern(input_line, pattern, latest_token_satisfied):
         else:
             input_line = result
             pattern = remaining_pattern
-            at_start = False  # after consuming first check
+            at_start = False
     return pattern == ""
 
 

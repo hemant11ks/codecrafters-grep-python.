c414f06650423e3e03de06a948bdbc40bf804a20
hemant11ks <ksingh1617@gmail.com>
2025-08-27 13:43:54 +0530

    added logic for | fixinggg

--- app/main.py
+++ app/main.py
@@ -1,189 +1,205 @@
-import math
 import sys
- 
- # import re
- # import pyparsing - available if you need it!
- # import lark - available if you need it!
- 
- 
-def find_first_match_index(
-     input_line: str, pattern: str, start_flag: bool = False, end_flag: bool = False
- ) -> int:
-     """
-     Finds the index of the first match of the given pattern in the input_line.
-     If no match is found, returns -1.
- 
-     Parameters:
-         input_line (str): The text to search in.
-         pattern (str): The pattern to search for.
- 
-     Returns:
-         int: The index of the first match, or -1 if no match is found.
-     """
- 
-     # Check of end of line.
-     if pattern == "$" and input_line == "":
-         return 0
- 
-     # Check for patterns like \\d and \\w
-     if pattern in ("\\d", "\\w"):
-         for idx, char in enumerate(input_line):
-             if (pattern == "\\d" and char.isdigit()) or (
-                 pattern == "\\w" and (char.isalpha() or char.isdigit())
-             ):
-                 if not (start_flag) or (start_flag and idx == 0):
-                     return idx + 1
-     # Check for patterns like [abc] or [^abc]
-     elif pattern[0] == "[" and pattern[-1] == "]":
-         if pattern[1] == "^":
-             negative_pattern = pattern[2:-1]
-             for idx, c in enumerate(negative_pattern):
-                 if c in input_line:
-                     if not (start_flag) or (start_flag and idx == 0):
-                         return -1
-             else:
-                 return 0
-         else:
-             positive_pattern = pattern[1:-1]
-             for idx, c in enumerate(positive_pattern):
-                 idx = input_line.find(c)
-                 if idx != -1:
-                     if not (start_flag) or (start_flag and idx == 0):
-                         return idx + 1
-     # Check for regular characters
-     else:
-         idx = input_line.find(pattern)
-         if idx >= 0:
-             if not (start_flag) or (start_flag and idx == 0):
-                 return idx + 1
- 
-     return -1
- 
- 
-def match_pattern_sequence(input_line: str, pattern: str) -> bool:
-     """
-     Check if input_line matches pattern. This method is called recursively.
- 
-     Parameters:
-         input_line (str): Text to check.
-         pattern (str): patterns.
- 
-     Returns:
-         bool: True if input_line matches pattern, False otherwise.
-     """
- 
-     start_flag = False
-     end_flag = False
-     while pattern:
- 
-         # Check for start flag
-         if pattern[0] == "^":
-             start_flag = True
-             pattern = pattern[1:]
- 
-         # Check for end flag
-         if pattern[-1] == "$":
-             end_flag = True
- 
-         # Check for alternation
-         if pattern[0] == "(":
-             closing_index = pattern.find(")")
-             pipe_index = pattern.find("|")
-             if closing_index == -1:
-                 raise ValueError(f"Alternation is not closed.{pattern}")
-             if pipe_index == -1:
-                 break
- 
-             alternations_str, pattern = (
-                 pattern[1:closing_index],
-                 pattern[closing_index + 1 :],
-             )
-             alternations = alternations_str.split("|")
-             print(alternations)
-             for alt in alternations:
-                 if match_pattern_sequence(input_line, alt):
-                     break
-             else:
-                 return False
- 
-         if len(pattern) == 0:
-            return True
 
-         # Get the current pattern to match
-         if pattern[0] == "\\":
-             current_pattern, pattern = pattern[:2], pattern[2:]
-         elif pattern[0] == "[":
-             closing_index = pattern.find("]") + 1
-             if closing_index == 0:
-#                raise ValueError("Closing not found")
-                raise ValueError(f"Character group is not closed.{pattern}")
-             current_pattern, pattern = pattern[:closing_index], pattern[closing_index:]
-         else:
-             current_pattern, pattern = pattern[:1], pattern[1:]
- 
-         if pattern and pattern[0] in ("+", "?", "."):
-             q_mode, pattern = pattern[0], pattern[1:]
-             match_len = 0
-             while True:
-                 input_start_pos = find_first_match_index(
-                     input_line, current_pattern, start_flag, end_flag
-                 )
- 
-                 if input_start_pos < 0:
-                     if q_mode == "+":
-                         if match_len > 0:
-                             break
-                         else:
-                             return False
-                     elif q_mode == "?":
-                         break
-                 else:
-                     match_len += 1
-                     input_line = input_line[input_start_pos:]
- 
-                     if q_mode in (".", "?") and match_len == 1:
-                         break
- 
-         else:
-             input_start_pos = find_first_match_index(
-                 input_line, current_pattern, start_flag, end_flag
-             )
- 
-             if input_start_pos < 0:
-                 return False
-             input_line = input_line[input_start_pos:]
- 
-     # Return True once whole pattern is checked without returning False
-     return True
- 
- 
+# import pyparsing - available if you need it!
+# import lark - available if you need it!
+
+
+def match(original_input_line, input_line, pattern):
+    if pattern[0] == "^":
+        return match_pattern(original_input_line, input_line, pattern[1:])
+    for i in range(len(input_line)):
+        if match_pattern(original_input_line, input_line[i:], pattern):
+            return True
+    return False
+
+
+def match_plus(original_input_line, input_line, pattern, pattern_idx):
+    # if pattern[0] != "." and input_line[0] != pattern[0]:
+    #     return False
+
+    return match_pattern(original_input_line, input_line[1:], pattern) or match_pattern(
+        original_input_line,
+        input_line[1:],
+        pattern[pattern_idx:],
+    )
+
+
+group_str = {}
+groups = []
+
+
+def match_pattern(original_input_line, input_line, pattern):
+    if not pattern:
+        return True
+
+    if not input_line:
+        return pattern in ["$", ")"]
+
+    if pattern[0] == ")":
+        if not groups:
+            return False
+        group_end_idx = len(original_input_line) - len(input_line)
+        group_number, group_start_idx = groups.pop()
+        group_str[group_number] = original_input_line[group_start_idx:group_end_idx]
+        return match_pattern(
+            original_input_line,
+            input_line,
+            pattern[1:],
+        )
+
+    if pattern[0] == "(":
+        right_paren_idx = 1
+        for i in range(1, len(pattern)):
+            right_paren_idx = i
+            if pattern[i] == ")":
+                break
+
+        group_number = len(group_str) + 1
+        group_str.setdefault(group_number, "")
+        group_start_idx = len(original_input_line) - len(input_line)
+        groups.append((group_number, group_start_idx))
+
+        if "|" in pattern[1:right_paren_idx]:
+            for group_pattern in pattern[1:right_paren_idx].split("|"):
+                if match_pattern(
+                    original_input_line,
+                    input_line,
+                    group_pattern + pattern[right_paren_idx:],
+                ):
+                    return True
+        else:
+            if match_pattern(
+                original_input_line,
+                input_line,
+                pattern[1:],
+            ):
+                return True
+
+        return False
+
+    if pattern[0] == "\\" and pattern[1].isdigit():
+        group_pattern = group_str[int(pattern[1])]
+        return match_pattern(
+            original_input_line,
+            input_line,
+            group_pattern + pattern[2:],
+        )
+
+    if len(pattern) > 1 and pattern[1] == "+":
+        if pattern[0] != "." and input_line[0] != pattern[0]:
+            return False
+        return match_plus(original_input_line, input_line, pattern, 2)
+
+    if len(pattern) > 1 and pattern[1] == "?":
+        if input_line[0] == pattern[0]:
+            return match_pattern(
+                original_input_line,
+                input_line[1:],
+                pattern[2:],
+            )
+
+        return match_pattern(original_input_line, input_line, pattern[2:])
+
+    if pattern[0] == "." or pattern[0] == input_line[0]:
+        return match_pattern(
+            original_input_line,
+            input_line[1:],
+            pattern[1:],
+        )
+
+    if pattern[:2] == "\\d" and input_line[0].isdigit():
+        if len(pattern) > 2 and pattern[2] == "+":
+            return match_plus(
+                original_input_line,
+                input_line,
+                pattern,
+                3,
+            )
+        return match_pattern(
+            original_input_line,
+            input_line[1:],
+            pattern[2:],
+        )
+
+    if pattern[:2] == "\\w" and (input_line[0].isalnum() or input_line[0] == "_"):
+        if len(pattern) > 2 and pattern[2] == "+":
+            return match_plus(
+                original_input_line,
+                input_line,
+                pattern,
+                3,
+            )
+        return match_pattern(
+            original_input_line,
+            input_line[1:],
+            pattern[2:],
+        )
+
+    if pattern[0] == "[":
+        right_bracket_idx = 1
+        for i in range(1, len(pattern)):
+            right_bracket_idx = i
+            if pattern[i] == "]":
+                break
+        if pattern[1] == "^":
+            if (
+                input_line[0].isalnum()
+                and input_line[0] not in pattern[2:right_bracket_idx]
+            ):
+                if (
+                    right_bracket_idx < len(pattern) - 1
+                    and pattern[right_bracket_idx + 1] == "+"
+                ):
+                    return match_plus(
+                        original_input_line,
+                        input_line,
+                        pattern,
+                        right_bracket_idx + 2,
+                    )
+                return match_pattern(
+                    original_input_line,
+                    input_line[1:],
+                    pattern[right_bracket_idx + 1 :],
+                )
+        else:
+            if input_line[0] in pattern[1:right_bracket_idx]:
+                if (
+                    right_bracket_idx < len(pattern) - 1
+                    and pattern[right_bracket_idx + 1] == "+"
+                ):
+                    return match_plus(
+                        original_input_line,
+                        input_line,
+                        pattern,
+                        right_bracket_idx + 2,
+                    )
+                return match_pattern(
+                    original_input_line,
+                    input_line[1:],
+                    pattern[right_bracket_idx + 1 :],
+                )
+    return False
+
+
 def main():
-     pattern = sys.argv[2]
-     input_line = sys.stdin.read()
- 
-     if sys.argv[1] != "-E":
-         print("Expected first argument to be '-E'")
-         exit(1)
- 
-     if match_pattern_sequence(input_line, pattern):
-         exit(0)
-     else:
-         exit(1)
- 
- 
-def main():
-     pattern = sys.argv[2]
-     input_line = sys.stdin.read()
- 
-     if sys.argv[1] != "-E":
-         print("Expected first argument to be '-E'")
-         exit(1)
- 
-     if match_pattern_sequence(input_line, pattern):
-         exit(0)
-     else:
-         exit(1)
- 
- 
+    pattern = sys.argv[2]
+    input_line = sys.stdin.read()
+
+    if sys.argv[1] != "-E":
+        print("Expected first argument to be '-E'")
+        exit(1)
+
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+    # echo -n "abc-def is abc-def, not efg, abc, or def" | ./your_program.sh -E "(([abc]+)-([def]+)) is \1, not ([^xyz]+), \2, or \3"
+    # input_line = "abc-def is abc-def, not efg, abc, or def"
+    # pattern = "(([abc]+)-([def]+)) is \\1, not ([^xyz]+), \\2, or \\3"
+    # Uncomment this block to pass the first stage
+    if match(input_line, input_line, pattern):
+        exit(0)
+    else:
+        exit(1)
+
+
 if __name__ == "__main__":
     main()

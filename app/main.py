eb4358e7f4aba1586aad28f2ace8ac8e4e82176f
hemant11ks <ksingh1617@gmail.com>
2025-08-27 13:00:24 +0530

    added logic for start anchor

--- app/main.py
+++ app/main.py
@@ -5,39 +5,44 @@ DIGIT = 1
 ALNUM = 2
 POS_CHAR_GROUP = 3
 NEG_CHAR_GROUP = 4
+START_ANCHOR = 5   # NEW
 
 numbers = list("0123456789")
 alphanum = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
 
 
-# Matches exactly one instance of next_token
-# Returns False or what's left of the input
-def match_one(input_line, token, pattern):
+def match_one(input_line, token, pattern, at_start):
     if token == DIGIT:
-        if input_line[0] in numbers:
+        if input_line and input_line[0] in numbers:
             return input_line[1:]
         else:
             return False
     elif token == ALNUM:
-        if input_line[0] in alphanum:
+        if input_line and input_line[0] in alphanum:
             return input_line[1:]
         else:
             return False
     elif token == LITERAL:
-        if input_line[0] == pattern[0]:
+        if input_line and input_line[0] == pattern[0]:
             return input_line[1:]
         else:
             return False
     elif token == POS_CHAR_GROUP:
         inclusion = pattern[1 : pattern.index("]")]
-        if input_line[0] in list(inclusion):
+        if input_line and input_line[0] in list(inclusion):
             return input_line[1:]
         else:
             return False
     elif token == NEG_CHAR_GROUP:
         exclusion = pattern[2 : pattern.index("]")]
-        if input_line[0] not in list(exclusion):
+        if input_line and input_line[0] not in list(exclusion):
             return input_line[1:]
+        else:
+            return False
+    elif token == START_ANCHOR:
+        # Only matches at very beginning
+        if at_start:
+            return input_line
         else:
             return False
     else:
@@ -51,31 +56,31 @@ def determine_next_token(pattern):
         return ALNUM, pattern[2:]
     elif len(pattern) >= 2 and pattern[:2] == "[^":
         return NEG_CHAR_GROUP, pattern[pattern.index("]") + 1 :]
-    elif pattern[0] == "[":
+    elif pattern and pattern[0] == "[":
         return POS_CHAR_GROUP, pattern[pattern.index("]") + 1 :]
+    elif pattern and pattern[0] == "^":
+        return START_ANCHOR, pattern[1:]
     else:
         return LITERAL, pattern[1:]
 
 
-# Can match multiple of next_token
-def match_multiple(input_line, token, pattern):
-    result = match_one(input_line, token, pattern)
-    return False
-
-
 def match_pattern(input_line, pattern, latest_token_satisfied):
-    while input_line:
+    at_start = True  # Track if we are at beginning
+    while True:
         if pattern == "":
             return True
+        if input_line == "" and pattern != "":
+            return False
+
         next_token, remaining_pattern = determine_next_token(pattern)
-        if input_line == "" and remaining_pattern == "" and latest_token_satisfied:
-            return True
-        result = match_one(input_line, next_token, pattern)
-        if result == False:
-            input_line = input_line[1:]
+        result = match_one(input_line, next_token, pattern, at_start)
+
+        if result is False:
+            return False
         else:
             input_line = result
             pattern = remaining_pattern
+            at_start = False  # after consuming first check
     return pattern == ""
 
 
